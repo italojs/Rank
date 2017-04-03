57aed65dc6659246c820a4fbe1f67570d3c9918c
italo jose <italo@sacapp.net>
2017-04-03 20:06:32 -0300

    First Commit

---
+++ Back.Api.Positivo.LineInfo/Business/LineInfoBSS.cs
@@ -0,0 +1,37 @@
+﻿using Back.Api.Positivo.LineInfo.DAL;
+using Back.Api.Positivo.LineInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Back.Api.Positivo.LineInfo.Business
+{
+    public class LineInfoBSS
+    {
+        private int _custumerId;
+        public LineInfoBSS(int customerId)
+        {
+            _custumerId = customerId;
+        }
+
+        public ILineInfo getLineInfo()
+        {
+
+            ILineInfo lineInfo = null;
+
+            try
+            {
+                IDataBase db = new DatabaseODBC();
+                lineInfo = LineInfoModel.Builder.Build(db, _custumerId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return lineInfo;
+        }
+    }
+}
---
+++ Back.Api.Positivo.LineInfo/DAL/IDataBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+
+namespace Back.Api.Positivo.LineInfo.DAL
+{
+    public interface IDataBase
+    {
+        void IExecNonQueryProc(DbCommand commands);
+        List<Dictionary<string, object>> IOExecProc(DbCommand commands);
+        List<object> OExecProc(DbCommand commands);
+    }
+}
